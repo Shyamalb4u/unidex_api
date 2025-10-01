5f16e5ff0ddcc6daaeb69443ae78191ed2516812
Shyamal <info@bigsss.com>
2025-10-01 17:09:33 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const sql = require("mssql");
 
 const dbconfig = require("./dbconfig");
 const userRouter = require("./routes/main");
-//const schedule = require("node-schedule"); changes in .env
+//const schedule = require("node-schedule");
 
 const app = express();
 app.use(bodyParser.json());
